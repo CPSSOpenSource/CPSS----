8aa4412ec3f9b1e749a014745c74d012cc403584
ComputerDeer <32599113+Ruskonert@users.noreply.github.com>
2018-07-23 12:51:28 +0900

    Update tool.py

--- tool.py
+++ tool.py
@@ -1,1 +1,9 @@
-import sys
+import zipfile
+import os
+
+if __name__ == "__main__":
+  zip = zipfile.Zipfile("cpss_ppt.zip", 'w')
+  zip.write(os.path.join(folder, file), file, compress_type = zipfile.ZIP_DEFLATED)
+  for folder, subfolders, files in os.walk('./python'):
+    zip.write(files, compress_type = zipfile.ZIP_DEFLATED)
+  zip.close()
